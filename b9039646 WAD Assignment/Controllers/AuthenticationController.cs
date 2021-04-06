cb7448841a04b9f303bfcf99224c895e5ec3435f
b90396 <b9039646"my.shu.ac.uk>
2021-04-06 16:53:08 +0000

    Implemented sign in page - can now register as a user and this will be stored in the database

---
+++ b9039646 WAD Assignment/Controllers/AuthenticationController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using b9039646_WAD_Assignment.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace b9039646_WAD_Assignment.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+
+        private readonly UserManager<User> userManager;
+        private readonly RoleManager<Role> roleManager;
+        private readonly SignInManager<User> signinManager;
+
+        public AuthenticationController(UserManager<User> userManager, RoleManager<Role> roleManager,
+            SignInManager<User> signinManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+            this.signinManager = signinManager;
+        }
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(Register obj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!roleManager.RoleExistsAsync("Admin").Result)
+                {
+                    Role role = new Role();
+                    role.Name = "Admin";
+                    IdentityResult roleResult = roleManager.
+                    CreateAsync(role).Result;
+                }
+
+                User user = new User();
+                user.UserName = obj.UserName;
+                user.Email = obj.Email;
+                //user.BirthDate = obj.BirthDate;
+
+                IdentityResult result = userManager.CreateAsync
+                (user, obj.Password).Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Admin").Wait();
+                    return RedirectToAction("SignIn", "Authentication");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid user details");
+                }
+            }
+            return View(obj);
+        }
+
+
+    }
+}
