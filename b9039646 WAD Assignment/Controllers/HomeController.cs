06b8f77e159b401c78d888f554c4984808dfbc45
b90396 <b9039646"my.shu.ac.uk>
2021-04-14 17:09:03 +0100

    can now delete favourite locations + some css improvements

--- b9039646 WAD Assignment/Controllers/HomeController.cs
+++ b9039646 WAD Assignment/Controllers/HomeController.cs
@@ -100,6 +100,15 @@ namespace b9039646_WAD_Assignment.Controllers
             {
                 list = _context.FavouriteLocations.Find(id);
                 model.location1 = _context.Locations.Find(list.locationCoordinates1);
+                model.location2 = _context.Locations.Find(list.locationCoordinates2);
+                model.location3 = _context.Locations.Find(list.locationCoordinates3);
+                model.location4 = _context.Locations.Find(list.locationCoordinates4);
+                model.location5 = _context.Locations.Find(list.locationCoordinates5);
+                model.location6 = _context.Locations.Find(list.locationCoordinates6);
+                model.location7 = _context.Locations.Find(list.locationCoordinates7);
+                model.location8 = _context.Locations.Find(list.locationCoordinates8);
+                model.location9 = _context.Locations.Find(list.locationCoordinates9);
+                model.location10 = _context.Locations.Find(list.locationCoordinates10);
             }
             else 
             {
@@ -123,6 +132,111 @@ namespace b9039646_WAD_Assignment.Controllers
                 _context.FavouriteLocations.Find(id).locationCoordinates1 = GeographicalCoordinates;
                 _context.SaveChanges();
             }
+            else if(_context.FavouriteLocations.Find(id).locationCoordinates2 == null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates2 = GeographicalCoordinates;
+                _context.SaveChanges();
+            }
+            else if (_context.FavouriteLocations.Find(id).locationCoordinates3 == null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates3 = GeographicalCoordinates;
+                _context.SaveChanges();
+            }
+            else if (_context.FavouriteLocations.Find(id).locationCoordinates4 == null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates4 = GeographicalCoordinates;
+                _context.SaveChanges();
+            }
+            else if (_context.FavouriteLocations.Find(id).locationCoordinates5 == null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates5 = GeographicalCoordinates;
+                _context.SaveChanges();
+            }
+            else if (_context.FavouriteLocations.Find(id).locationCoordinates6 == null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates6 = GeographicalCoordinates;
+                _context.SaveChanges();
+            }
+            else if (_context.FavouriteLocations.Find(id).locationCoordinates7 == null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates7 = GeographicalCoordinates;
+                _context.SaveChanges();
+            }
+            else if (_context.FavouriteLocations.Find(id).locationCoordinates8 == null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates8 = GeographicalCoordinates;
+                _context.SaveChanges();
+            }
+            else if (_context.FavouriteLocations.Find(id).locationCoordinates9 == null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates9 = GeographicalCoordinates;
+                _context.SaveChanges();
+            }
+            else if (_context.FavouriteLocations.Find(id).locationCoordinates10 == null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates10 = GeographicalCoordinates;
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("FavouriteLocationsPage");
+        }
+
+        public IActionResult DeleteFavourite(int index)
+        {
+            string locationToDelete = "locationCoordinates" + index;
+            var id = _userManager.GetUserId(User);
+
+            if (index == 1 && _context.FavouriteLocations.Find(id).locationCoordinates1 != null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates1 = null;
+                _context.SaveChanges();
+            }
+            if (index == 2 && _context.FavouriteLocations.Find(id).locationCoordinates2 != null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates2 = null;
+                _context.SaveChanges();
+            }
+            if (index == 3 && _context.FavouriteLocations.Find(id).locationCoordinates3 != null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates3 = null;
+                _context.SaveChanges();
+            }
+            if (index == 4 && _context.FavouriteLocations.Find(id).locationCoordinates4 != null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates4 = null;
+                _context.SaveChanges();
+            }
+            if (index == 5 && _context.FavouriteLocations.Find(id).locationCoordinates5 != null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates5 = null;
+                _context.SaveChanges();
+            }
+            if (index == 6 && _context.FavouriteLocations.Find(id).locationCoordinates6 != null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates6 = null;
+                _context.SaveChanges();
+            }
+            if (index == 7 && _context.FavouriteLocations.Find(id).locationCoordinates7 != null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates7 = null;
+                _context.SaveChanges();
+            }
+            if (index == 8 && _context.FavouriteLocations.Find(id).locationCoordinates8 != null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates8 = null;
+                _context.SaveChanges();
+            }
+            if (index == 9 && _context.FavouriteLocations.Find(id).locationCoordinates9 != null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates9 = null;
+                _context.SaveChanges();
+            }
+            if (index == 10 && _context.FavouriteLocations.Find(id).locationCoordinates10 != null)
+            {
+                _context.FavouriteLocations.Find(id).locationCoordinates10 = null;
+                _context.SaveChanges();
+            }
+
 
             return RedirectToAction("FavouriteLocationsPage");
         }
--- b9039646 WAD Assignment/wwwroot/js/site.js
+++ b9039646 WAD Assignment/wwwroot/js/site.js
@@ -20,6 +20,17 @@ function dropDown() {
 
 }
 
-function sizeOnHover() {
+var slideIndex = 0;
+showSlides();
 
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("Slides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000);
 }
